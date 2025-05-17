55214bcede71c09a99e3545a402c3743bda31596
Maki325 <kedecasa@gmail.com>
2025-05-17 00:05:22 -0400

    commit Header

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,10 +1,10 @@
-
-import './App.css';
+import Home from "./Pages/Home/Home";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello</h1>
+      <Home />
     </div>
   );
 }
